a271b2c2db22c39e87a38ff6dc55462941681b5b
erenturkan9090-ui <erenturkan9090@gmail.com>
2026-04-12 21:27:17 +0300

    docs: add CoordinatorDashboard test entry to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,17 @@
 import RegisterPage from './pages/RegisterPage'
+import CoordinatorDashboard from './pages/CoordinatorDashboard' 
 import './App.css'
 
 function App() {
   return (
     <div className="app">
+      
       <RegisterPage />
+
+      
     </div>
   )
 }
 
 export default App
 
-
-/* =========================================
-   TEST İÇİN KULLANDIĞIMIZ KODLAR
-   İleride sayfayı tekrar test eedecekseniz 
-   yukarıdaki App fonksiyonunu yoruma alıp 
-   buradakini ve import'unu açın
-   =========================================
-
-import CoordinatorDashboard from './pages/CoordinatorDashboard';
-
-function App() {
-  return (
-    <div>
-      <CoordinatorDashboard />
-    </div>
-  );
-}
-*/

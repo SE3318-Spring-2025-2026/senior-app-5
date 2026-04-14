98c9d72d1878308535f6e35ec101b2ae0ea5952d
Cemil Gökay Pekşen <23070006028@stu.yasar.edu.tr>
2026-04-14 23:41:06 +0300

    Add ApiTags decorator to PhasesController

--- backend/src/phases/phases.controller.ts
+++ backend/src/phases/phases.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Param, Put, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
+import { Role } from '../auth/enums/role.enum';
 import { PhasesService } from './phases.service';
 import { UpdatePhaseScheduleDto } from './dto/update-phase-schedule.dto';
 
+@ApiTags('Phases')
 @Controller('phases')
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Roles(Role.Coordinator)

1d08d1ffdf0fd01619c6f03b7a24f6f859c610ab
Cemil Gökay Pekşen <23070006028@stu.yasar.edu.tr>
2026-04-14 23:33:08 +0300

    Add ApiProperty decorator to submissionStart

--- backend/src/phases/dto/update-phase-schedule.dto.ts
+++ backend/src/phases/dto/update-phase-schedule.dto.ts
@@ -1,6 +1,9 @@
 import { IsDateString } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class UpdatePhaseScheduleDto {
+  
+  @ApiProperty()
+  
   @IsDateString()
   submissionStart!: string;
 
